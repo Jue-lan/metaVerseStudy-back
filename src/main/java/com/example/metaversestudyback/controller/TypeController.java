672de0acf7c4f002794e216ceb971f5f4467f461
jue-lan <juelan.brown@nyu.edu>
2022-02-04 10:55:39 -0500

    had ephifany about cors issuse

--- src/main/java/com/example/metaversestudyback/controller/TypeController.java
+++ src/main/java/com/example/metaversestudyback/controller/TypeController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.logging.Logger;
 
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping(path = "/api")
 public class TypeController {
